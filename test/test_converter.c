cc72afb35800a4c1de2285d1b74aff35cf84ea8d
Julien Lespagnol <lespagnolj@gmail.com>
2024-03-02 09:50:56 +0100

    first batch of test before implementing the logic

--- test/test_converter.c
+++ test/test_converter.c
@@ -8,6 +8,7 @@
  * @copyright Copyright (c) 2024
  *
  */
+#include "string.h"
 #include "unity.h"
 
 #include "converter.h"
@@ -36,11 +37,35 @@ void test_error_return(void)
     TEST_ASSERT_EQUAL_INT(expected_result, result);
 }
 
-TEST_CASE(1, 2)
-TEST_CASE(10, 11)
-TEST_CASE(21, 22)
-TEST_CASE(21, 23)
-void test_parametrized(int a, int b)
+TEST_CASE(1, "I\n")
+TEST_CASE(2, "II\n")
+TEST_CASE(5, "V\n")
+TEST_CASE(10, "X\n")
+TEST_CASE(20, "XX\n")
+TEST_CASE(50, "L\n")
+TEST_CASE(100, "C\n")
+TEST_CASE(500, "D\n")
+TEST_CASE(1000, "M\n")
+TEST_CASE(1562, "MDLXII\n")
+
+TEST_CASE(4, "IV\n")
+TEST_CASE(9, "IV\n")
+TEST_CASE(40, "XL\n")
+TEST_CASE(90, "XC\n")
+TEST_CASE(400, "CD\n")
+TEST_CASE(900, "CM\n")
+
+void test_parametrized(int integer, char *expected_roman_s)
 {
-    TEST_ASSERT_EQUAL_INT(b, (a + 1));
+    int result = 0;
+    int expected_result = 0;
+    char *roman_s = NULL;
+    int expected_roman_s_len = 0;
+
+    expected_roman_s_len = strlen(expected_roman_s);
+
+    result = integer_to_roman(integer, &roman_s);
+    TEST_ASSERT_NOT_NULL(roman_s);
+    TEST_ASSERT_EQUAL_INT(expected_result, result);
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected_roman_s, roman_s, expected_roman_s_len);
 }
