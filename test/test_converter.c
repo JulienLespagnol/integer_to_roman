dfadeccd46a0ae0ee7971dffa1e76af90e21464b
Julien Lespagnol <lespagnolj@gmail.com>
2024-03-02 09:43:09 +0100

    Just playing with the test framework, time to work

--- test/test_converter.c
+++ test/test_converter.c
@@ -8,17 +8,39 @@
  * @copyright Copyright (c) 2024
  *
  */
-
 #include "unity.h"
 
 #include "converter.h"
 
+#define TEST_CASE(...)
+#define TEST_RANGE(...)
+
 void test_error_malloc(void)
 {
-    int integer = 0;
+    int integer = 1;
     int result = 0;
     char *roman_s = NULL;
 
     result = integer_to_roman(integer, &roman_s);
     TEST_ASSERT_NOT_NULL(roman_s);
+}
+
+void test_error_return(void)
+{
+    int integer = 1;
+    int expected_result = 0;
+    int result = 0;
+    char *roman_s = NULL;
+
+    result = integer_to_roman(integer, &roman_s);
+    TEST_ASSERT_EQUAL_INT(expected_result, result);
+}
+
+TEST_CASE(1, 2)
+TEST_CASE(10, 11)
+TEST_CASE(21, 22)
+TEST_CASE(21, 23)
+void test_parametrized(int a, int b)
+{
+    TEST_ASSERT_EQUAL_INT(b, (a + 1));
 }
